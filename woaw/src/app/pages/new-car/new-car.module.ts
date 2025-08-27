0b469b6f3c5e92ddd2442c28e90f61a5af90ff89
SebastianAndoney <becario.sebastian@mideservices.com>
2025-08-27 17:23:20 -0600

    cambios en el formulario y validaciones

--- woaw/src/app/pages/new-car/new-car.module.ts
+++ woaw/src/app/pages/new-car/new-car.module.ts
@@ -1,22 +1,22 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
-
 import { IonicModule } from '@ionic/angular';
 
 import { NewCarPageRoutingModule } from './new-car-routing.module';
-
 import { NewCarPage } from './new-car.page';
 
-// importamos la navbar
+// Componentes compartidos
 import { NavbarComponent } from '../../components/navbar/navbar.component';
 import { CartasComponent } from '../../components/cartas/cartas.component';
 import { AcomodoComponent } from '../../components/filtos/acomodo/acomodo.component';
 import { FooterComponent } from '../../components/footer/footer.component';
 import { AddComponent } from '../../components/lote/add/add.component';
+
+// Formularios existentes
 import { CarComponent } from '../../components/new-veiculo/car/car.component';
 import { MotosComponent } from '../../components/new-veiculo/motos/motos.component';
-/* import {} */
+import { CamionComponent } from '../../components/new-veiculo/camion/camion.component';
 
 @NgModule({
   imports: [
@@ -24,14 +24,19 @@ import { MotosComponent } from '../../components/new-veiculo/motos/motos.compone
     FormsModule,
     IonicModule,
     NewCarPageRoutingModule,
+
+    // compartidos
     NavbarComponent,
     CartasComponent,
     AcomodoComponent,
     FooterComponent,
+    AddComponent,
+
+    // formularios
     CarComponent,
     MotosComponent,
-    AddComponent
+    CamionComponent, // <- necesario para usar <app-camion> en new-car.html
   ],
   declarations: [NewCarPage],
 })
-export class NewCarPageModule { }
+export class NewCarPageModule {}
