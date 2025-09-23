9218d5841a01f6875d7a5d5ea8d7da5a8b53275e
andoney701-code <andoney701@gmail.com>
2025-09-23 16:56:03 -0600

    Filtro camiones (#86)
    done, endpoin

--- woaw/src/app/pages/renta-ciudades/renta-ciudades.module.ts
+++ woaw/src/app/pages/renta-ciudades/renta-ciudades.module.ts
@@ -5,18 +5,15 @@ import { FormsModule } from '@angular/forms';
 import { IonicModule } from '@ionic/angular';
 
 import { RentaCiudadesPageRoutingModule } from './renta-ciudades-routing.module';
-
 import { RentaCiudadesPage } from './renta-ciudades.page';
 import { NavbarComponent } from '../../components/navbar/navbar.component';
 import { FooterComponent } from '../../components/footer/footer.component';
- 
- 
 
 @NgModule({
   imports: [
     CommonModule,
     FormsModule,
-    IonicModule,
+    IonicModule, 
     RentaCiudadesPageRoutingModule,
     NavbarComponent,
     FooterComponent
--- woaw/src/app/pages/renta-coches/renta-coches.page.ts
+++ woaw/src/app/pages/renta-coches/renta-coches.page.ts
@@ -375,6 +375,7 @@ export class RentaCochesPage implements OnInit, OnDestroy {
     }
 
 
+
     if (this.vistaActiva === 'todos') {
       this.todosFiltrados = lista;
       this.totalTodos = this.todosFiltrados.length;
